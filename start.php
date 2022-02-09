ec64b43bb0c69b48310e082c756a0db851723a90
MD. ALL SHAHORIAR TONMOY <78097293+AST-TheCoder@users.noreply.github.com>
2022-02-09 22:27:50 +0600

    Update start.php

--- start.php
+++ start.php
@@ -13,7 +13,7 @@
     <label for="name">Name:</label><br>
     <input type="text" name="name" id="" placeholder="Enter your name" value="<?php echo $name?>" required>
     <label for="gr_size">Level (1 to 4):</label><br>
-    <input type="number" min="1" max="4" name="gr_sz" id="" placeholder="Enter your preferred" required>
+    <input type="number" min="1" max="4" name="gr_sz" id="" placeholder="Enter your preferred level" required>
     <br><br>
     <button name="start">Start</button><br><br>
   </form>
